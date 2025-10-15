580bcf99ef9d463bb328be9ee8184395c917db78
rajput-raj <rajsinghjnp15@gmail.com>
2025-10-15 19:55:45 +0530

    Updated project

--- src/data/skillsData.js
+++ src/data/skillsData.js
@@ -10,7 +10,7 @@
 ]
 
 // Choose your skills from below. Make sure it's in the same format and spelled correctly.
-// Couldn't find the required skills? Raise an issue on github at https://github.com/hhhrrrttt222111/developer-portfolio/issues/new
+// Couldn't find the required skills? Raise an issue on github at https://github.com/rajput-raj/issues/new
 
 
 // AVAILABLE SKILLS
